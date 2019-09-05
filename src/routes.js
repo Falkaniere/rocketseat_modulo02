d278f86ecb7065e1d992a9ad9a6c1ee599383366
Jonatas Falkaniere Soares <jonatasfalkaniere@gmai.com>
2019-09-04 23:11:28 -0300

    commite

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.post('/', async (req, res) => {
+  const user = await User.create({
+    name: 'diego fernandes',
+    email: 'diego@rocketseat',
+    password_hash: '123',
+  });
+
+  return res.json(user);
+});
+
+export default routes;
